4e99f7ca5715f70aa8a861fe64970e1fd5814392
ahmadnasaiei <ahmad.nasaiei@gmail.com>
2023-03-05 05:36:43 +0800

    navbar update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import NavBar from './components/nav/navbar'
+import Home from './components/Home/home'
 const App = () => {
   return (
     <>
       <NavBar />
+      <Home />
     </>
   )
 }
--- src/components/nav/navbar.jsx
+++ src/components/nav/navbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import './navbar.css'
 import logo from '../../assets/logo.png'
+import { useState } from 'react';
 
 const navbar = () => {
+
+  // to change burger classes
+  const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
+  const [isMenuClicked, setIsMenuClicked] = useState(false)
+
+  // toggle burger menu change
+  const updateMenu = () => {
+      if(!isMenuClicked) {
+          setBurgerClass("burger-bar clicked")
+      }
+      else {
+          setBurgerClass("burger-bar unclicked")
+      }
+      setIsMenuClicked(!isMenuClicked)
+  }
+
   const links = [
     {
       id: 1,
@@ -23,16 +40,41 @@ const navbar = () => {
   ]
 
   return (
-    <div className="flex items-center justify-between h-20 w-full px-72 py-14 fixed">
-      <div className='w-32'>
-        <img src={logo} alt="" />
-      </div>  
-      <ul className='flex justify-end'>
+    <div className="main flex justify-between items-center py-8 px-5 md:w-9/12 ml-auto mr-auto">
+      
+      <div className='block w-32 h-auto cursor-pointer z-10'>
+        <a href="#"><img src={logo} alt="" /></a>
+      </div>
+
+      <ul className='hidden md:flex'>
         {links.map(({ id, link }) => (
-          <li key={id} className='px-10 cursor-pointer uppercase tracking-widest hover:text-ascent  duration-150'>{link}</li>
+          <li key={id} className='px-10 cursor-pointer uppercase tracking-widest hover:text-ascent duration-150'>{link}</li>
         ))}
       </ul>
-      <button className='px-6 py-2 border-2 border-ascent text-ascent rounded-full hover:bg-ascent hover:text-bg duration-150'>RESUME</button>
+
+      <button className='hidden md:flex px-6 py-2 border-2 border-ascent text-ascent rounded-full hover:bg-ascent hover:text-bg duration-150'>RESUME</button>
+
+      <div className='md:hidden z-10'>
+        <div className="burger-menu" onClick={updateMenu}>
+          <div className={burger_class}></div>
+          <div className={burger_class}></div>
+          <div className={burger_class}></div>
+        </div>
+      </div>
+
+      {isMenuClicked && (
+        <>
+        <ul className='flex md:hidden flex-col justify-center items-center absolute top-0 right-0 w-2/3 h-screen bg-primary text-light'>
+          {links.map(({ id, link }) => (
+            <li key={id} className='py-6 pl-6 text-xl cursor-pointer uppercase tracking-widest hover:text-ascent duration-150'>{link}</li>
+          ))}
+          <li className='py-6 pl-6 text-xl cursor-pointer uppercase tracking-widest'><button className='md:flex px-6 py-2 border-2 border-ascent text-ascent rounded-full hover:bg-ascent hover:text-primary duration-150'>RESUME</button></li>
+        </ul>
+        </>
+      )}
+
+      
+      
     </div>
   )
 }
